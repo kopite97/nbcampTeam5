67b5f12198e9ca371faacb1a8947ce9bb14c4b7c
leesewon <leesw1945@gmail.com>
2024-05-07 19:14:44 +0900

    Scoree Class 구현

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -1,15 +1,38 @@
 package camp.model;
 
 public class Score {
-    private String scoreId;
+    private String account;
+    private int studentAccount;
+    private int round;
+    private int score;
+    private ScoreRank scoreRank;
 
-    public Score(String seq) {
-        this.scoreId = seq;
+    public Score(String account, int studentAccount, int round, int score, ScoreRank scoreRank) {
+        this.account = account;
+        this.studentAccount = studentAccount;
+        this.round = round;
+        this.score = score;
+        this.scoreRank = scoreRank;
     }
 
     // Getter
-    public String getScoreId() {
-        return scoreId;
+    public String account() {
+        return account;
     }
 
+    public int studentAccount() {
+        return studentAccount;
+    }
+
+    public int round() {
+        return round;
+    }
+
+    public int score() {
+        return score;
+    }
+
+    public ScoreRank scoreRank() {
+        return scoreRank;
+    }
 }
