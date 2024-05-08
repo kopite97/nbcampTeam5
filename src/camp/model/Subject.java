71de9d7cd20b3063e979c2787f7f596f90fc576f
moony <78634780+ysy56@users.noreply.github.com>
2024-05-08 20:11:29 +0900

    feat[Subject]: subjectType 필드 및 getter 메서드 추가

--- src/camp/model/Subject.java
+++ src/camp/model/Subject.java
@@ -2,13 +2,19 @@ package camp.model;
 
 public class Subject {
     private final String subjectId;
+    private final SubjectType subjectType;
 
-    public Subject(String seq, SubjectType subjectType) {
-        this.subjectId = seq;
+    public Subject(String subjectId, SubjectType subjectType) {
+        this.subjectId = subjectId;
+        this.subjectType = subjectType;
     }
 
     // Getter
     public String getSubjectId() {
         return subjectId;
+    }
+
+    public SubjectType getSubjectType() {
+        return subjectType;
     }
 }
